d3f8065051befd1ea1ac34110f5ffb4f8f564630
shaw <shaw-wei@foxmail.com>
2026-05-20 11:13:53 +0800

    feat(risk-control): 内容审计新增关键词拦截
    (cherry picked from commit 91da815993732e6536be8c702168822e482cd850)

--- backend/internal/handler/admin/content_moderation_handler.go
+++ backend/internal/handler/admin/content_moderation_handler.go
@@ -44,6 +44,8 @@ type contentModerationConfigRequest struct {
 	HitRetentionDays     *int      `json:"hit_retention_days"`
 	NonHitRetentionDays  *int      `json:"non_hit_retention_days"`
 	PreHashCheckEnabled  *bool     `json:"pre_hash_check_enabled"`
+	BlockedKeywords      *[]string `json:"blocked_keywords"`
+	KeywordBlockingMode  *string   `json:"keyword_blocking_mode"`
 }
 
 type contentModerationAPIKeyTestRequest struct {
@@ -99,6 +101,8 @@ func (h *ContentModerationHandler) UpdateConfig(c *gin.Context) {
 		HitRetentionDays:     req.HitRetentionDays,
 		NonHitRetentionDays:  req.NonHitRetentionDays,
 		PreHashCheckEnabled:  req.PreHashCheckEnabled,
+		BlockedKeywords:      req.BlockedKeywords,
+		KeywordBlockingMode:  req.KeywordBlockingMode,
 	})
 	if err != nil {
 		response.ErrorFrom(c, err)
